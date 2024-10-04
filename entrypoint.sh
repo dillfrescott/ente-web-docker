858704ca1c915a930b2fd82b97c7fee7e5c544fe
Cross <cross@dill.moe>
2024-10-04 05:20:29 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd /ente/web
 
 PORT=3000 NEXT_PUBLIC_ENTE_ENDPOINT=https://photos.dill.moe \
     NEXT_PUBLIC_ENTE_ALBUMS_ENDPOINT=https://albums.dill.moe \
-    yarn dev:photos
+    yarn dev:photos &
 
 PORT=3010 NEXT_PUBLIC_ENTE_ENDPOINT=https://photos.dill.moe \
     NEXT_PUBLIC_ENTE_ALBUMS_ENDPOINT=https://albums.dill.moe \
