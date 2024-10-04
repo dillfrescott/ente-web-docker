ac6e906931f62367dd79e18f3c8fa803edcbe460
Cross Nastasi <cross@dill.moe>
2024-10-04 05:58:27 -0400

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 NEXT_PUBLIC_ENTE_ENDPOINT=https://photos.dill.moe \
-    NEXT_PUBLIC_ENTE_ALBUMS_ENDPOINT=https://albums.dill.moe \
+    NEXT_PUBLIC_ENTE_ALBUMS_ENDPOINT=https://photos-web.dill.moe \
     serve /ente/web/apps/photos/out --port 3000 &
-
-NEXT_PUBLIC_ENTE_ENDPOINT=https://photos.dill.moe \
-    NEXT_PUBLIC_ENTE_ALBUMS_ENDPOINT=https://albums.dill.moe \
-    serve /ente/web/apps/photos/out --port 3002
